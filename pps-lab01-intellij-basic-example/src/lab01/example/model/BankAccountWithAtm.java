d6ab7f1317631c5780cc69aba14a68209ca8eb0c
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-02-28 16:40:44 +0100

    Add comments to the BankAccountWithAtm interface

--- pps-lab01-intellij-basic-example/src/lab01/example/model/BankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/BankAccountWithAtm.java
@@ -2,7 +2,23 @@ package lab01.example.model;
 
 public interface BankAccountWithAtm extends BankAccount {
 
+    /**
+     * Allows the deposit of an amount on the account using an ATM, if the given userID corresponds to the register holder ID
+     * of the bank account. This ID acts like an "identification token" .
+     * The total amount to be deposited is taxed of a transaction fee.
+     *
+     * @param id the id of the user that wants do the deposit
+     * @param amount the amount of the deposit
+     */
     void depositWithAtm(int id, double amount);
 
+    /**
+     * Allows the withdrawal of an amount from the account using an ATM, if the given userID corresponds to the register holder ID
+     * of the bank account. This ID acts like an "identification token" .
+     * The total amount to be deposited is taxed of a transaction fee.
+     *
+     * @param id the id of the user that wants do the withdrawal
+     * @param amount the amount of the withdrawal
+     */
     void withdrawWithAtm(int id, double amount);
 }
